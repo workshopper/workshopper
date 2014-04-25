09917691826c46ab56e1c35e750eb1f9d6948212
imakewebthings <caleb@imakewebthings.com>
2014-04-24 20:16:18 -0700

    Add reset option for clearing workshop progress

--- workshopper.js
+++ workshopper.js
@@ -118,6 +118,11 @@ function Workshopper (options) {
     return this.execute(argv._[0], argv._.slice(1))
   }
 
+  if (argv._[0] == 'reset') {
+    this.reset()
+    return console.log(this.title + ' progress reset')
+  }
+
   this.printMenu()
 }
 
@@ -319,6 +324,15 @@ Workshopper.prototype.updateData = function (id, fn) {
 
   file = path.resolve(this.dataDir, id + '.json')
   fs.writeFileSync(file, JSON.stringify(fn(json)))
+}
+
+Workshopper.prototype.reset = function () {
+  this.updateData('completed', function () {
+    return []
+  })
+  this.updateData('current', function () {
+    return null
+  })
 }
 
 
