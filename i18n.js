48c46c42c4d63c3a50e916834a62cfa4cf57c0a7
Martin Heidegger <martin.heidegger@gmail.com>
2014-12-19 23:34:14 +0900

    Extracted i18n methods to own file

---
+++ i18n.js
@@ -0,0 +1,59 @@
+const i18nCore   = require('i18n-core')
+    , i18nFs     = require('i18n-core/lookup/fs')
+    , i18nObject = require('i18n-core/lookup/object')
+    , path       = require('path')
+
+function i18nChain() {
+  var linked = {
+        handler: arguments[0]
+      , next: null
+    }
+    , current = linked
+  for (var i = 1; i<arguments.length; i++) {
+    var next = {
+      handler: arguments[i]
+    }
+    current.next = next
+    current = next
+  }
+  return {
+    get: function (key) {
+      var current = linked
+        , result
+      while (!result && current) {
+        result = current.handler.get(key)
+        current = current.next
+      }
+      if (!result)
+        return "?" + key + "?"
+      
+      return result
+    }
+  }
+}
+
+function createDefaultLookup(options, exercises) {
+  var result = {
+    en: {
+        title: options.title
+      , subtitle: options.subtitle
+      , exercise: {}
+    }
+  }
+
+  exercises.forEach(function (exercise) {
+    result.en.exercise[exercise] = exercise
+  })
+
+  return result
+}
+
+module.exports = function(options, exercises) {
+  return i18nCore(
+    i18nChain(
+        i18nFs(options.appDir)
+      , i18nFs(path.resolve(__dirname, './i18n'))
+      , i18nObject(createDefaultLookup(options, exercises))
+    )
+  )
+}
--- workshopper.js
+++ workshopper.js
@@ -5,9 +5,6 @@ const argv       = require('optimist').argv
     , map        = require('map-async')
     , msee       = require('msee')
     , chalk      = require('chalk')
-    , i18nCore   = require('i18n-core')
-    , i18nFs     = require('i18n-core/lookup/fs')
-    , i18nObject = require('i18n-core/lookup/object')
 
 
 const showMenu         = require('./menu')
@@ -17,51 +14,6 @@ const showMenu         = require('./menu')
 
 
 const defaultWidth = 65
-
-function i18nChain() {
-  var linked = {
-        handler: arguments[0]
-      , next: null
-    }
-    , current = linked
-  for (var i = 1; i<arguments.length; i++) {
-    var next = {
-      handler: arguments[i]
-    }
-    current.next = next
-    current = next
-  }
-  return {
-    get: function (key) {
-      var current = linked
-        , result
-      while (!result && current) {
-        result = current.handler.get(key)
-        current = current.next
-      }
-      if (!result)
-        return "?" + key + "?"
-      
-      return result
-    }
-  }
-}
-
-function createDefaultLookup(options, exercises) {
-  var result = {
-    en: {
-        title: options.title
-      , subtitle: options.subtitle
-      , exercise: {}
-    }
-  }
-
-  exercises.forEach(function (exercise) {
-    result.en.exercise[exercise] = exercise
-  })
-
-  return result
-}
 
 function Workshopper (options) {
   if (!(this instanceof Workshopper))
@@ -125,13 +77,7 @@ function Workshopper (options) {
   })
 
 
-  var i18n_base = i18nCore(
-      i18nChain(
-        i18nFs(options.appDir),
-        i18nFs(path.resolve(__dirname, './i18n')),
-        i18nObject(createDefaultLookup(options, this.exercises))
-      )
-    )
+  var i18n_base = require("./i18n")(options, this.exercises)
     , i18n = i18n_base.lang(lang, true)
     , __ = i18n.__
     , __n = i18n.__n
